873082ccdddbaf36311a36b0642cbb2c06cee937
root <root@localhost.localdomain>
2018-05-06 12:55:04 -0300

    creando repositorio

---
+++ src/main/java/br/com/eodontodigital/eodontodigitalAPI/EodontodigitalApiApplication.java
@@ -0,0 +1,20 @@
+package br.com.eodontodigital.eodontodigitalAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class EodontodigitalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EodontodigitalApiApplication.class, args);
+	}
+
+	@GetMapping("/")
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
+	
+}
