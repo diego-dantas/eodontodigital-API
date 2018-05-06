4e543421efc576578ac73884e6b52dbd289801a0
root <root@localhost.localdomain>
2018-05-06 13:17:58 -0300

    a

---
+++ src/main/java/br/com/eodontodigital/eodontodigitalAPI/security/PasswordSecurity.java
@@ -0,0 +1,22 @@
+package br.com.eodontodigital.eodontodigitalAPI.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordSecurity {
+
+    public static String getPasswod(String password){
+
+        if(password == null) return password;
+
+        BCryptPasswordEncoder bCryptPassword = new BCryptPasswordEncoder();
+        return bCryptPassword.encode(password);
+    }
+
+    public static boolean validatePassword(String password, String passwordEncoded){
+
+        if(password == null || passwordEncoded == null) return false;
+
+        BCryptPasswordEncoder bCryptPassword = new BCryptPasswordEncoder();
+        return bCryptPassword.matches(password, passwordEncoded);
+    }
+}
