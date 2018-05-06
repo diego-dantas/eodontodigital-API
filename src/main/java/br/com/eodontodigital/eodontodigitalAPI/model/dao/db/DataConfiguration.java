780bfd222c32b6f6cb12e67eecbcf2d76a313cc3
Diego Dantas <Dantas@localhost.localdomain>
2018-05-06 16:26:24 -0300

    a

--- src/main/java/br/com/eodontodigital/eodontodigitalAPI/model/dao/db/DataConfiguration.java
+++ src/main/java/br/com/eodontodigital/eodontodigitalAPI/model/dao/db/DataConfiguration.java
@@ -1,44 +1,44 @@
 package br.com.eodontodigital.eodontodigitalAPI.model.dao.db;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import javax.sql.DataSource;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.jdbc.datasource.DriverManagerDataSource;
+// import org.springframework.orm.jpa.JpaVendorAdapter;
+// import org.springframework.orm.jpa.vendor.Database;
+// import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+// import javax.sql.DataSource;
 
-@Configuration
+//@Configuration
 public class DataConfiguration {
 
-    @Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    // @Bean
+    // public DataSource dataSource(){
+    //     DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        //Parametos de conficuração do banco de dados
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        // dataSource.setUrl("jdbc:mysql://courses-api-202517:us-central1:eodontodigital-db");
-        // dataSource.setUsername("eodontodigital-db");
-        // dataSource.setPassword("102030");
-        dataSource.setUrl("jdbc:mysql://mysql.visualmidia.info:3306/visualmidia05");
-        dataSource.setUsername("visualmidia05");
-        dataSource.setPassword("senha102030");
-    //    dataSource.setUrl("jdbc:mysql://localhost:3306/eodontodigitaldb");
-    //    dataSource.setUsername("root");
-    //    dataSource.setPassword("");
-        return dataSource;
-    }
+    //     //Parametos de conficuração do banco de dados
+    //     dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    //     // dataSource.setUrl("jdbc:mysql://courses-api-202517:us-central1:eodontodigital-db");
+    //     // dataSource.setUsername("eodontodigital-db");
+    //     // dataSource.setPassword("102030");
+    //     dataSource.setUrl("jdbc:mysql://mysql.visualmidia.info:3306/visualmidia05");
+    //     dataSource.setUsername("visualmidia05");
+    //     dataSource.setPassword("senha102030");
+    // //    dataSource.setUrl("jdbc:mysql://localhost:3306/eodontodigitaldb");
+    // //    dataSource.setUsername("root");
+    // //    dataSource.setPassword("");
+    //     return dataSource;
+    // }
 
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter(){
+    // @Bean
+    // public JpaVendorAdapter jpaVendorAdapter(){
 
-        //Parametros de configuração do Hibernate
-        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setDatabase(Database.MYSQL);
-        adapter.setShowSql(true);
-        adapter.setGenerateDdl(true);
-        adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
-        adapter.setPrepareConnection(true);
-        return adapter;
-    }
+    //     //Parametros de configuração do Hibernate
+    //     HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+    //     adapter.setDatabase(Database.MYSQL);
+    //     adapter.setShowSql(true);
+    //     adapter.setGenerateDdl(true);
+    //     adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+    //     adapter.setPrepareConnection(true);
+    //     return adapter;
+    // }
 }
--- src/main/java/br/com/eodontodigital/eodontodigitalAPI/security/PasswordSecurity.java
+++ src/main/java/br/com/eodontodigital/eodontodigitalAPI/security/PasswordSecurity.java
@@ -1,22 +1,22 @@
 package br.com.eodontodigital.eodontodigitalAPI.security;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordSecurity {
 
-    public static String getPasswod(String password){
+    // public static String getPasswod(String password){
 
-        if(password == null) return password;
+    //     if(password == null) return password;
 
-        BCryptPasswordEncoder bCryptPassword = new BCryptPasswordEncoder();
-        return bCryptPassword.encode(password);
-    }
+    //     BCryptPasswordEncoder bCryptPassword = new BCryptPasswordEncoder();
+    //     return bCryptPassword.encode(password);
+    // }
 
-    public static boolean validatePassword(String password, String passwordEncoded){
+    // public static boolean validatePassword(String password, String passwordEncoded){
 
-        if(password == null || passwordEncoded == null) return false;
+    //     if(password == null || passwordEncoded == null) return false;
 
-        BCryptPasswordEncoder bCryptPassword = new BCryptPasswordEncoder();
-        return bCryptPassword.matches(password, passwordEncoded);
-    }
+    //     BCryptPasswordEncoder bCryptPassword = new BCryptPasswordEncoder();
+    //     return bCryptPassword.matches(password, passwordEncoded);
+    // }
 }
